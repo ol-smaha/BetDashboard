9756d13e7a3100af2e53db74e5f23408044809d7
Oleh Smaha <oleh.smaha.work@gmail.com>
2023-10-16 18:47:15 +0300

    fix collectstatic

--- app/run_app.sh
+++ app/run_app.sh
@@ -2,6 +2,6 @@
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic   # need to gunicorn download static
+python manage.py collectstatic --noinput  # need to gunicorn download static
 gunicorn --access-logfile - --workers 4 --timeout 300 --reload \
   --bind app:8000 core.wsgi:application
