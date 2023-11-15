8697871dbf0ef104e42206c338c4da370540f1b8
Oleh Smaha <oleh.smaha.work@gmail.com>
2023-11-15 19:30:19 +0200

    BetBase History: add pagination, delete, change favourite

--- app/bet/urls.py
+++ app/bet/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 
 from bet.views import (BetHistoryView, BetGraphsView, Statistic, BetGraphsProfitView, BetGraphsResultView,
-                       FootballBetHistoryView, BetGraphsRoiView, BetCreate)
+                       FootballBetHistoryView, BetGraphsRoiView, BetCreateView, BetBaseChangeFavouriteStatusView,
+                       BetBaseDeleteView)
 
 urlpatterns = [
     path('history/', BetHistoryView.as_view(), name='bet_history'),
-    path('create/', BetCreate.as_view(), name='bet_create'),
+    path('create/', BetCreateView.as_view(), name='bet_create'),
+    path('delete/<int:id>', BetBaseDeleteView.as_view(), name='bet_delete'),
+    path('change_favourite_status/<int:id>', BetBaseChangeFavouriteStatusView.as_view(), name='bet_change_favourite_status'),
     path('graphs/', BetGraphsView.as_view(), name='bet_graphs'),
     path('graphs/profit/', BetGraphsProfitView.as_view(), name='bet_graphs_profit'),
     path('graphs/result/', BetGraphsResultView.as_view(), name='bet_graphs_result'),
