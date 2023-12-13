21d4670609b8c096adb4adad40cb4d74084e935f
Oleh Smaha <oleh.smaha.work@gmail.com>
2023-12-14 01:03:49 +0200

    nginx fix

--- app/core/settings.py
+++ app/core/settings.py
@@ -209,3 +209,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
