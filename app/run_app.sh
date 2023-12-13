6a69db71e9a25847ed8dd69b21312beebc99a705
Oleh Smaha <oleh.smaha.work@gmail.com>
2023-12-14 01:45:41 +0200

    nginx fix

--- app/run_app.sh
+++ app/run_app.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations --noinput
 python manage.py migrate
 python manage.py collectstatic --noinput  # need to gunicorn download static
 gunicorn --access-logfile - --workers 4 --timeout 120 --reload \
-  --bind app:8000 core.wsgi:application
+  --bind app:8443 core.wsgi:application
