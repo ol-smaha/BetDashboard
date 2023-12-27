64d00578c561f5a568445a198f342493f223b12e
Oleh Smaha <oleh.smaha.work@gmail.com>
2023-12-27 15:41:26 +0200

    small ui fixes

--- app/core/settings.py
+++ app/core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
-ALLOWED_HOSTS = os.environ.get('SERVERNAMES').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
