525ed86fe482474b302039b05c9507341337fba9
Oleh Smaha <oleh.smaha.work@gmail.com>
2023-12-27 16:06:35 +0200

    fix DEBUG

--- app/core/settings.py
+++ app/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ['DEBUG_VALUE'] == 'TRUE'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
